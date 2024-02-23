f9693b5918d8363c881510a0a0b4a2289d9b2d76
rutgerhanze <152165901+rutgerhanze@users.noreply.github.com>
2024-02-23 17:16:21 +0100

    make method stubs

--- src/main/java/namen_sorteren/project/controller/NameFileProcessor.java
+++ src/main/java/namen_sorteren/project/controller/NameFileProcessor.java
@@ -9,61 +9,19 @@ import java.util.Scanner;
 public class NameFileProcessor {
 
     public static int countNamesInFile(String fileName) {
-        int nameCounter = 0;
-        try {
-            File file = new File(fileName);
-            Scanner readFile = new Scanner(file);
-            while (readFile.hasNext()) {
-                readFile.next();
-                nameCounter++;
-            }
-        } catch (FileNotFoundException exception) {
-            System.out.println("Bestand niet gevonden.");
-        }
-        return nameCounter;
+        return 0;
     }
 
     public static ArrayList<String> readNamesOfFile(String fileName) {
-        ArrayList<String> nameList = new ArrayList<>();
-        try {
-            File file = new File(fileName);
-            Scanner readFile = new Scanner(file);
-            while (readFile.hasNext()) {
-                nameList.add(readFile.next());
-            }
-        } catch (FileNotFoundException fout) {
-            System.out.println("Bestand niet gevonden.");
-        }
-        return nameList;
+        return new ArrayList<>();
     }
 
     public static void AddNameToSortedList(String name, ArrayList<String> list) {
-        int index = 0;
-        while (name.compareTo(list.get(index)) > 0) {
-            index++;
-            if (index >= list.size()) {
-                list.add(name);
-                return;
-            }
-        }
-        list.add(index, name);
     }
 
     public static void addListToSortedList(ArrayList<String> newList, ArrayList<String> oldList) {
-        for (String name : newList) {
-            AddNameToSortedList(name, oldList);
-        }
     }
 
     public static void makeFileFromList(ArrayList<String> list, String fileName) {
-        try {
-            PrintWriter printWriter = new PrintWriter(fileName);
-            for (String name : list) {
-                printWriter.println(name);
-            }
-            printWriter.close();
-        } catch (Exception Exception) {
-            System.out.println(Exception.getMessage());
-        }
     }
 }
--- src/main/java/namen_sorteren/project/controller/NamenSorterenLauncher.java
+++ src/main/java/namen_sorteren/project/controller/NamenSorterenLauncher.java
@@ -12,17 +12,5 @@ public class NamenSorterenLauncher {
 
     public static void main(String[] args) {
 
-        System.out.printf("Het bestand bevat %d namen\n", countNamesInFile(NAME_FILE_OLD));
-
-        ArrayList<String> namenLijstOrigineel = readNamesOfFile(NAME_FILE_OLD);
-        System.out.println(namenLijstOrigineel);
-
-        AddNameToSortedList("José", namenLijstOrigineel);
-        AddNameToSortedList("Theo", namenLijstOrigineel);
-        ArrayList<String> namenLijstExtra = readNamesOfFile(NAME_FILE_EXTRA);
-        System.out.println(namenLijstExtra);
-        addListToSortedList(namenLijstExtra, namenLijstOrigineel);
-        System.out.println(namenLijstOrigineel);
-        makeFileFromList(namenLijstOrigineel, NAME_FILE_NEW);
     }
 }
