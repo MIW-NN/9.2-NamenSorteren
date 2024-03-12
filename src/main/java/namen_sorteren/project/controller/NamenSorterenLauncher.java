e5ead0cc1a0140913f1f30586cfbaab23f2acee0
rutgerhanze <152165901+rutgerhanze@users.noreply.github.com>
2024-03-12 13:03:42 +0100

    point path to files in resource

--- src/main/java/namen_sorteren/project/controller/NamenSorterenLauncher.java
+++ src/main/java/namen_sorteren/project/controller/NamenSorterenLauncher.java
@@ -6,28 +6,24 @@ import static namen_sorteren.project.controller.NameFileProcessor.*;
 
 
 public class NamenSorterenLauncher {
-    private final static String NAME_FILE_OLD = "9.2 GesorteerdeNamenOrigineel.txt";
-    private final static String NAME_FILE_EXTRA = "9.2 ExtraNamen.txt";
-    private final static String NAME_FILE_NEW = "9.2 GesorteerdeNamenNieuw.txt";
+    private final static String NAME_FILE_OLD = "src/main/resources/9.2 GesorteerdeNamenOrigineel.txt";
+    private final static String NAME_FILE_EXTRA = "src/main/resources/9.2 ExtraNamen.txt";
+    private final static String NAME_FILE_NEW = "src/main/resources/9.2 GesorteerdeNamenNieuw.txt";
 
 
     public static void main(String[] args) throws URISyntaxException {
-        var classloader = Thread.currentThread().getContextClassLoader();
-        var pathToExtraNames = classloader.getResource(NAME_FILE_EXTRA).toURI().getPath();
-        var pathToOldFile = classloader.getResource(NAME_FILE_OLD).toURI().getPath();
-        var pathToNewFile= classloader.getResource(NAME_FILE_NEW).toURI().getPath();
 
-        System.out.printf("Het bestand bevat %d namen\n", countNamesInFile(pathToOldFile));
+        System.out.printf("Het bestand bevat %d namen\n", countNamesInFile(NAME_FILE_OLD));
 
-        ArrayList<String> namenLijstOrigineel = readNamesOfFile(pathToOldFile);
+        ArrayList<String> namenLijstOrigineel = readNamesOfFile(NAME_FILE_OLD);
         System.out.println(namenLijstOrigineel);
 
         AddNameToSortedList("José", namenLijstOrigineel);
         AddNameToSortedList("Theo", namenLijstOrigineel);
-        ArrayList<String> namenLijstExtra = readNamesOfFile(pathToExtraNames);
+        ArrayList<String> namenLijstExtra = readNamesOfFile(NAME_FILE_EXTRA);
         System.out.println(namenLijstExtra);
         addListToSortedList(namenLijstExtra, namenLijstOrigineel);
         System.out.println(namenLijstOrigineel);
-        makeFileFromList(namenLijstOrigineel, pathToNewFile);
+        makeFileFromList(namenLijstOrigineel, NAME_FILE_NEW);
     }
 }
