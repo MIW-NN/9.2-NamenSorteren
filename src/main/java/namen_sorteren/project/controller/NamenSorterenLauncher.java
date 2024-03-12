a783d8b7795cfa88ba9ff5365cec5efa63b69af8
rutgerhanze <152165901+rutgerhanze@users.noreply.github.com>
2024-03-12 13:04:15 +0100

    point path to files in resources

--- src/main/java/namen_sorteren/project/controller/NamenSorterenLauncher.java
+++ src/main/java/namen_sorteren/project/controller/NamenSorterenLauncher.java
@@ -6,16 +6,12 @@ import static namen_sorteren.project.controller.NameFileProcessor.*;
 
 
 public class NamenSorterenLauncher {
-    private final static String NAME_FILE_OLD = "9.2 GesorteerdeNamenOrigineel.txt";
-    private final static String NAME_FILE_EXTRA = "9.2 ExtraNamen.txt";
-    private final static String NAME_FILE_NEW = "9.2 GesorteerdeNamenNieuw.txt";
+    private final static String NAME_FILE_OLD = "src/main/resources/9.2 GesorteerdeNamenOrigineel.txt";
+    private final static String NAME_FILE_EXTRA = "src/main/resources/9.2 ExtraNamen.txt";
+    private final static String NAME_FILE_NEW = "src/main/resources/9.2 GesorteerdeNamenNieuw.txt";
+
 
 
     public static void main(String[] args) throws URISyntaxException {
-        var classloader = Thread.currentThread().getContextClassLoader();
-        var pathToExtraNames = classloader.getResource(NAME_FILE_EXTRA).toURI().getPath();
-        var pathToOldFile = classloader.getResource(NAME_FILE_OLD).toURI().getPath();
-        var pathToNewFile= classloader.getResource(NAME_FILE_NEW).toURI().getPath();
-
     }
 }
